5c013d2ffe77f41e77495104e9bf772f832d7fb5
Nmuaddib <nelsonsrj@hotmail.com>
2017-09-08 14:12:43 -0300

    Gráficos de faixa etária

--- Analise_QT_VL_Beneficiário/faixa etaria.R
+++ Analise_QT_VL_Beneficiário/faixa etaria.R
@@ -8,10 +8,49 @@ x_vv_faixa <- read_excel('beneficiarios_faixa.xlsx') %>%
                                           "54 a 58 anos", "59 anos ou mais"))) %>% 
   select(ANO, ANOMES, FAIXA, BENEFICIARIOS = BT)
 
-
-
 vv_faixa_ano <- x_vv_faixa %>% 
   group_by(ANO, FAIXA) %>% 
   summarise(BENEFICIARIOS.md = mean(BENEFICIARIOS)) %>% 
   as.data.frame() %>% 
-  mutate(TOTAL_B = vv_ano[match(.$ANO, vv_ano$ANO), 13][[1]])
+  mutate(TOTAL_B = vv_ano[match(.$ANO, vv_ano$ANO), 13][[1]]) %>% 
+  mutate(P_FAIXA = round((BENEFICIARIOS.md/TOTAL_B)*100,3))
+
+ggplot(data = vv_faixa_ano, aes(x = ANO)) +
+  geom_bar(aes(y = BENEFICIARIOS.md, group = FAIXA, fill = FAIXA), 
+           width = 0.9, alpha = 0.75, 
+           stat = 'identity',
+           position = 'fill') +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1)) +
+  scale_fill_brewer(palette = 'Spectral',
+                    direction = 1) +
+  labs(title = 'Percentual de faixa etária por ano',
+       x = 'Ano', 
+       y = 'Percentual') +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle = 45, 
+                                   vjust = 1, 
+                                   size = 9, 
+                                   hjust = 1))
+
+ggplot(data = x_vv_faixa , aes(x = ANOMES)) +
+  geom_line(aes(y = BENEFICIARIOS, color = FAIXA, group = FAIXA), 
+            size = 1, alpha = 1, show.legend = T) +
+  scale_y_continuous(breaks = seq(10000, 200000, by = 5000)) +
+  scale_colour_manual(values = c('blue',
+                                 'yellow',
+                                 'green',
+                                 'red',
+                                 'orange',
+                                 'dark green',
+                                 'black',
+                                 'gray',
+                                 'violet',
+                                 'light blue')) +
+  labs(title = 'Evolução da faixa etária',
+       x = 'Ano e mês', 
+       y = 'Beneficiários') +
+  theme_gray() +
+  theme(axis.text.x = element_text(angle = 45, 
+                                   vjust = 1, 
+                                   size = 9, 
+                                   hjust = 1))
