a39c60195e33cd0cd17e8bf96f0a082a6214c849
Nmuaddib <nelsonsrj@hotmail.com>
2020-01-15 20:34:20 +0000

    Tese, server

--- Tese MIDxPT/Main Draft.R
+++ Tese MIDxPT/Main Draft.R
@@ -15,7 +15,7 @@ library("forcats")
 #ds_appprg <- read_excel("Dados Aplicativos com Programa.xlsx")
 #ds_patprg <- read_excel("Dados Patentes com Programa.xlsx")
 #ds_prdprg <- read_excel("Dados Produtos com Programa.xlsx")
-#ds_frmdic <- read_excel("Formação dos discentes 2013-2016 - Tese.xlsx")
+ds_frmdic <- read_excel("Formação dos discentes 2013-2016 - Tese.xlsx")
 #ds_frmdoc <- read_excel("Formação dos docentes tese.xlsx")
 ds_prj013 <- read_excel("Membros dos projetos 2013.xlsx")
 #ds_prj014 <- read_excel("Membros dos projetos 2014.xlsx")
@@ -24,8 +24,8 @@ ds_prj013 <- read_excel("Membros dos projetos 2013.xlsx")
 ds_prgppg <- read_excel("Programas PPG 2016.xlsx")
 ds_prjppg <- read_excel("Projetos PPG 2016.xlsx")
 
-ls(ds_prgppg)
-ls(ds_prjppg)
+# ls(ds_prgppg)
+# ls(ds_prjppg)
 
 ds_pgpj <- merge(ds_prjppg, ds_prgppg, by = "CD_PROGRAMA_IES", no.dups = T) %>% 
   mutate(ANO_PRJ = str_sub(.$DH_INICIO, 1, 4)) %>% 
@@ -36,14 +36,14 @@ ds_pgpj <- merge(ds_prjppg, ds_prgppg, by = "CD_PROGRAMA_IES", no.dups = T) %>%
          CD_PROGRAMA_IES, 
          NM_PROGRAMA_IES = NM_PROGRAMA_IES.x, 
          ID_PROJETO, DS_PROJETO) %>% 
-  filter(ANO_PRJ >= 2013, ) %>% 
+  filter(ANO_PRJ >= 2013) %>% 
   group_by(NM_AREA_AVALIACAO, NM_PROGRAMA_IES) %>% 
   summarise(QT_PRJ = n_distinct(ID_PROJETO)) %>% 
   arrange(NM_AREA_AVALIACAO, desc(QT_PRJ))
 
 write.csv2(ds_pgpj, "Qt_Projetos por Programa 2013-2016.csv")
 
-rm(ds_pgpj)
+# rm(ds_pgpj)
 
 ds_anos_prj <- ds_prjppg %>% 
   mutate(ANO_PRJ = str_sub(.$DH_INICIO, 1, 4)) %>%
@@ -52,7 +52,47 @@ ds_anos_prj <- ds_prjppg %>%
 
 write.csv2(ds_anos_prj, "Projetos por ano inicio do programa.csv")
 
-?filter
-?stringr
+##### Filtro projetos de 2013-2016
+
+ds_prjppg_13_16 <- ds_prjppg %>% 
+  mutate(ANO_PRJ = str_sub(.$DH_INICIO, 1, 4)) %>%
+  filter(ANO_PRJ >= 2013)
+
+##### Filtro de membros discentes e junção com Projetos
+
+ds_prj013_dis <- ds_prj013 %>%  arrange(DS_TIPO_MEMBRO) %>% 
+  filter(DS_TIPO_MEMBRO == "DISCENTE") %>% 
+  merge(., ds_prjppg_13_16, by = "ID_PROJETO")
+
+# ls(ds_prj013_dis)
+# ls(ds_frmdic)
+# ls(ds_disprjfrm)
+# ?merge
+
+##### Filtra nivel de formação 
+
+ds_frmdic_grad <- ds_frmdic %>% filter(nivel_formacao == "Graduação")
 
 
+##### Junção de Formação (graduação) com membros de projeto (discentes, 2013-2016) por Nome+ID Programa
+
+ds_disprjfrm <- merge(ds_prj013_dis, ds_frmdic_grad, 
+                      by.x = c("CD_PROGRAMA_IES", "NM_MEMBRO_PROJETO"), 
+                      by.y = c("cod_programa", "nome_filtro_cvlattes"))
+
+# ds_dis <- ds_prj013_dis %>% select(CD_PROGRAMA_IES, NM_MEMBRO_PROJETO)
+# ds_frm <- ds_frmdic %>% select(cod_programa, nome_filtro_cvlattes)
+
+
+##### Quantidade de vinculos por Membro/Formação (Projetos??)
+
+ds_qt_disprjfrm <- ds_disprjfrm %>% 
+  group_by(AN_BASE.x,
+           NM_AREA_AVALIACAO,
+           nivel_formacao,
+           CD_PROGRAMA_IES, 
+           NM_PROGRAMA_IES, 
+           NM_MEMBRO_PROJETO,
+           nome_curso_formacao) %>% 
+  summarise(QT_VINC = n()) %>% 
+  arrange(desc(QT_VINC))
